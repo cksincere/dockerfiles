1e5d553077daddef5d36b64da6702dab10f6b412
Alex Lorca <alex.lorca@khipu.com>
2015-08-13 15:04:01 -0300

    add image: zabbix-agent

---
+++ zabbix-agent/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "entrypoint.sh <env> <etcd addr:port> <zabbix hostname>"
+    exit 1
+fi
+
+KHENV=$1
+ETCD_NODE=$2
+export ZABBIX_HOSTNAME=$3
+
+confd -onetime -backend etcd -node "$ETCD_NODE"  -prefix="/khipu/$KHENV/" -confdir /etc/confd || exit 1
+
+exec gosu khipu zabbix_agentd -f -c /etc/zabbix_agentd.conf
