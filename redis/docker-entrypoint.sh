e9121d04ed9a47b76f47a4d0fda32ff34aecb086
Alex Lorca <alex.lorca@khipu.com>
2016-06-02 16:44:53 -0400

    redis: config generated at runtime (master/slave/sentinel)

---
+++ redis/docker-entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+: ${KHIPU_ENV:?"must be set"}
+: ${ETCD_PEERS:?"must be set"}
+
+MODE=""
+
+case "$1" in
+    redis-master)
+        MODE=master
+        ;;
+    redis-slave)
+        MODE=slave
+        ;;
+    redis-sentinel)
+        MODE=sentinel
+        ;;
+    *)
+        echo "mode not specified: redis-master|redis-slave|redis-sentinel"
+        exit 1
+        ;;
+esac
+
+shift
+
+if [ ! -f /data/redis.conf ]; then
+    CERTDIR=/etc/etcd/certs
+    PREFIX="/khipu/${KHIPU_ENV}/redis/${MODE}/"
+
+    if [ -f ${CERTDIR}/client.pem ] && [ -f ${CERTDIR}/client-key.pem ] && [ -f  ${CERTDIR}/ca.pem ]; then
+        confd -log-level debug -onetime -backend etcd -node https://${ETCD_PEERS} -scheme https -client-cert $CERTDIR/client.pem -client-key $CERTDIR/client-key.pem -prefix=${PREFIX}
+    else
+        confd -log-level debug -onetime -backend etcd -node http://${ETCD_PEERS} -prefix=${PREFIX}
+    fi
+    chown khipu:khipu /data/redis.conf
+fi
+
+case "$MODE" in
+    master|slave)
+        exec su-exec khipu redis-server /data/redis.conf "$@"
+        ;;
+    sentinel)
+        exec su-exec khipu redis-server /data/redis.conf --sentinel "$@"
+        ;;
+esac
